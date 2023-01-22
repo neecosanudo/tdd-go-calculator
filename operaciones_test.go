a6dee4e4588ed14513ea3d0e1e5202286a5efcaa
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2023-01-22 19:16:08 -0300

    ♻️ REFACTOR: extracting assert message function for Suma() test

--- operaciones_test.go
+++ operaciones_test.go
@@ -8,26 +8,27 @@ func TestSuma(t *testing.T) {
 		got := Suma(2, 5)
 		want := 7
 
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		assertMessageForSumaTest(t, got, want)
 	})
 
 	t.Run("varios números", func(t *testing.T) {
 		got := Suma(4, 5, 6, 10)
 		want := 25
 
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		assertMessageForSumaTest(t, got, want)
 	})
 
 	t.Run("números negativos", func(t *testing.T) {
 		got := Suma(-3, -7, 10)
 		want := 0
 
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		assertMessageForSumaTest(t, got, want)
 	})
+}
+
+func assertMessageForSumaTest(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
 }
